07847f80163427c897d97003904ab669499534c9
osmanoscar <osmankok701@gmail.com>
2024-01-19 10:05:47 +0300

    coenda la salle

--- app/build/tmp/kapt3/stubs/debug/com/kotlin/task/service/WeatherRetrofitService.java
+++ app/build/tmp/kapt3/stubs/debug/com/kotlin/task/service/WeatherRetrofitService.java
@@ -18,22 +18,12 @@ public abstract interface WeatherRetrofitService {
     java.lang.String apiKey, @org.jetbrains.annotations.NotNull()
     kotlin.coroutines.Continuation<? super retrofit2.Response<com.kotlin.task.model.WeatherData>> continuation);
     
-    @kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0006\b\u0086\u0003\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u0006\u0010\t\u001a\u00020\u0004R\u001c\u0010\u0003\u001a\u0004\u0018\u00010\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0005\u0010\u0006\"\u0004\b\u0007\u0010\b\u00a8\u0006\n"}, d2 = {"Lcom/kotlin/task/service/WeatherRetrofitService$Companion;", "", "()V", "retrofitService", "Lcom/kotlin/task/service/WeatherRetrofitService;", "getRetrofitService", "()Lcom/kotlin/task/service/WeatherRetrofitService;", "setRetrofitService", "(Lcom/kotlin/task/service/WeatherRetrofitService;)V", "getInstance", "app_debug"})
+    @kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\b\u0086\u0003\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u0006\u0010\u0005\u001a\u00020\u0004R\u0010\u0010\u0003\u001a\u0004\u0018\u00010\u0004X\u0082\u000e\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0006"}, d2 = {"Lcom/kotlin/task/service/WeatherRetrofitService$Companion;", "", "()V", "retrofitService", "Lcom/kotlin/task/service/WeatherRetrofitService;", "getInstance", "app_debug"})
     public static final class Companion {
-        @org.jetbrains.annotations.Nullable()
         private static com.kotlin.task.service.WeatherRetrofitService retrofitService;
         
         private Companion() {
             super();
-        }
-        
-        @org.jetbrains.annotations.Nullable()
-        public final com.kotlin.task.service.WeatherRetrofitService getRetrofitService() {
-            return null;
-        }
-        
-        public final void setRetrofitService(@org.jetbrains.annotations.Nullable()
-        com.kotlin.task.service.WeatherRetrofitService p0) {
         }
         
         @org.jetbrains.annotations.NotNull()
